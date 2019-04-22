6aa462da61c6764d33bb3de4b23244942adc4c6e
Lance Good <lgoodyt@gmail.com>
2019-04-22 13:25:57 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 Config.BoomBoom = {
-    Controls = {1, 51},
-    ControlsTitle = "E",
-    Cooldown = 15,
+    Controls        = {1, 51},  -- Go to https://docs.fivem.net/game-references/controls/ for more options.
+    ControlsTitle   = "E",      -- What button the message tells the user to press (display text).
+    Cooldown        = 15,       -- The length (in minutes) users must wait until using the command again.
 }
